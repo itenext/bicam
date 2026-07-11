082eedd75a75f36e453ca6d4288381a87c5d6d9c
itenext <159773204+itenext@users.noreply.github.com>
2026-07-11 08:38:33 +0000

    hiiii

--- bichem/wsgi.py
+++ bichem/wsgi.py
@@ -2,10 +2,9 @@
 WSGI config for bichem project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
+
+import os
 
 from django.core.wsgi import get_wsgi_application
 
